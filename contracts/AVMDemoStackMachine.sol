f8ce047b40367b2505e6d1cae2da01421034f3c4
Tim Goddard <tim.goddard@aurainfosec.com>
2016-09-21 20:43:50 +1200

    Defer updating registers until one final write (reduce write count), jump example

--- contracts/AVMDemoStackMachine.sol
+++ contracts/AVMDemoStackMachine.sol
@@ -9,14 +9,18 @@ contract AVMDemoStackMachine is AVMStepValidator {
     event BadWriteValue(uint, uint, uint);
     using AVMMemoryContext32 for AVMMemoryContext32.Context;
     
-    uint constant addrInstructionPointer = 0;
-    uint constant addrStackPointer = 4;
+    uint constant addrStatusRegister = 0;
+    uint constant mask32 = 4294967295;
+    uint constant offsetInstructionPointer = 26959946667150639794667015087019630673637144422540572481103610249216;
+    uint constant offsetStackPointer = 6277101735386680763835789423207666416102355444464034512896;
     
     function validateStep(uint256[] readAccesses, uint256[] writeAccesses) external returns (bool) {
         AVMMemoryContext32.Context memory ctx = AVMMemoryContext32.initContext(readAccesses, writeAccesses, getMemoryWordsLog2());
-        uint ip = ctx.read32(addrInstructionPointer);
-        uint sp = ctx.read32(addrStackPointer);
-        uint op = ctx.readByte(ip++);
+        uint256 statusRegister = ctx.read256(addrStatusRegister);
+        uint ip = (statusRegister / offsetInstructionPointer) & mask32;
+        uint sp = (statusRegister / offsetStackPointer) & mask32;
+        uint8 op = ctx.readByte(ip++);
+        
         uint v; uint w;
         if (op == 0x0) {
             // NOP
@@ -29,11 +33,9 @@ contract AVMDemoStackMachine is AVMStepValidator {
             trace("Value read from opcodes: ");
             traceNum(v);
             sp -= 4;
-            ctx.write32(addrStackPointer, sp);
             ctx.write32(sp, v);
         } else if (op == 0x2) { // POP
             sp += 4;
-            ctx.write32(addrStackPointer, sp);
         } else if (op == 0x3) { // LOAD
             v = ctx.read32(sp);
             v = ctx.read32(v);
@@ -42,31 +44,26 @@ contract AVMDemoStackMachine is AVMStepValidator {
             v = ctx.read32(sp);
             w = ctx.read32(sp + 4);
             sp += 8;
-            ctx.write32(addrStackPointer, sp);
             ctx.write32(v, w);
         } else if (op == 0x5) { // ADD
             v = ctx.read32(sp);
             w = ctx.read32(sp + 4);
             sp += 4;
-            ctx.write32(addrStackPointer, sp);
             ctx.write32(sp, v + w);
         } else if (op == 0x6) { // SUB
             v = ctx.read32(sp);
             w = ctx.read32(sp + 4);
             sp += 4;
-            ctx.write32(addrStackPointer, sp);
             ctx.write32(sp, v - w);
         } else if (op == 0x7) { // AND
             v = ctx.read32(sp);
             w = ctx.read32(sp + 4);
             sp += 4;
-            ctx.write32(addrStackPointer, sp);
             ctx.write32(sp, v & w);
         } else if (op == 0x8) { // OR
             v = ctx.read32(sp);
             w = ctx.read32(sp + 4);
             sp += 4;
-            ctx.write32(addrStackPointer, sp);
             ctx.write32(sp, v | w);
         } else if (op == 0x9) { // NOT
             v = ctx.read32(sp);
@@ -75,16 +72,18 @@ contract AVMDemoStackMachine is AVMStepValidator {
             v = ctx.read32(sp);
             w = ctx.read32(sp + 4);
             sp += 8;
-            ctx.write32(addrStackPointer, sp);
             if (v == 0) {
-                ip = w;
+                ip = (uint32) (w);
             }
         } else {
             // Unknown operation
             // Do not write anything, freeze here.
             return ctx.isValid();
         }
-        ctx.write32(addrInstructionPointer, ip);
+        
+        statusRegister = (ip * offsetInstructionPointer) | (sp * offsetStackPointer);
+        
+        ctx.write256(addrStatusRegister, statusRegister);
         return ctx.isValid();
     }
     
